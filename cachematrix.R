9c3ade4f3784a112c7ec0ac32cb039e86f34d707
sunshanqi <33873811+sunshanqi@users.noreply.github.com>
2017-12-02 11:04:32 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,20 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  matrixInverseCopy<-NULL      # Initialized matrixInverseCopy with NULL
+  set<-function(y){
+    x <<- y                     # Assign value of matrix i.e. 'x'
+    matrixInverseCopy <<- NULL         
+  }
+  get <- function() x        # get value of matrix i.e. 'x'
+  
+  # setInverse function calcualtes inverse of matrix 'x' and assign the 
+  # result to matrixInverseCopy
+  setInverse <- function (solve) matrixInverseCopy <<- solve
+  # getInverse function is used to get inverse of matrix 'x'
+  getInverse <- function() matrixInverseCopy
+  
+  list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
 }
 
 
@@ -12,4 +25,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        matrixInverse <- x$getInverse()  # Retrieve matrix inverse 
+  if(!is.null(matrixInverse)){   # If matrix inverse is calculated
+    message("Getting cached data: Matrix inverse is already calculated")
+    return(matrixInverse) # Return a matrix that is the inverse of 'x'
+  }
+  data <- x$get()      #  Retrieve a matrix using get method
+  matrixInverse <- solve(data, ...)  # Calculate inverse of matrix
+  x$setInverse(matrixInverse) # Set value of inverse of matrix
+  (matrixInverse)  # Return a matrix that is the inverse of 'x'
 }
